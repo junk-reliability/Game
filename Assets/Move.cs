701d4c0d9a7b9893c20b14814f1d3e6084548591
Skrals <39524682+Skrals@users.noreply.github.com>
2019-04-16 23:08:28 +0300

    Добавлено
    Сфера с контроллером, нужно исправить прыжок и сделать контролируемую камеру

---
+++ Assets/Move.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move : MonoBehaviour {
+
+    private const float V = 15f;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKey(KeyCode.W))
+        {
+            GetComponent<Rigidbody>().AddForce(Vector3.forward * V);
+        }
+        if (Input.GetKey(KeyCode.W) && Input.GetKey(KeyCode.LeftShift))
+        {
+            GetComponent<Rigidbody>().AddForce(Vector3.forward * V * 2);
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            GetComponent<Rigidbody>().AddForce(Vector3.back * V);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            GetComponent<Rigidbody>().AddForce(Vector3.right * V);
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            GetComponent<Rigidbody>().AddForce(Vector3.left * V);
+        }
+        if (Input.GetKey(KeyCode.Space))
+        {
+            GetComponent<Rigidbody>().AddForce(Vector3.up * V * 2);
+        }
+    }
+}
