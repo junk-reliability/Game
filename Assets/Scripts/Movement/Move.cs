7f813fd86d6d61f4e95470e9aab177041b5af55d
Skrals <39524682+Skrals@users.noreply.github.com>
2019-04-20 22:56:00 +0300

    изменения
    Добавлены текстуры и изменен скрипт для передвижения

---
+++ Assets/Scripts/Movement/Move.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move : MonoBehaviour {
+
+    private const float V = 15f;//скорость
+    private Rigidbody rb;// получаем тело 
+	// Use this for initialization
+	void Start ()
+    {
+        rb = GetComponent<Rigidbody>();// инициализируем тело
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        float V_Move = Input.GetAxis("Vertical");//вертикальное движение
+        float H_Move = Input.GetAxis("Horizontal");// горизонтальное движение
+        Vector3 V3Move = new Vector3(H_Move,0,V_Move);// создаем вектор 
+        rb.AddForce(V3Move * V);// Задаем движение по вектору
+    }
+}
