61ba0860650db00b4a190fa152726c106b66250f
Skrals <39524682+Skrals@users.noreply.github.com>
2019-04-25 20:47:17 +0300

    Sun script
    добавил вращение солнца

---
+++ Assets/Scripts/Sun/SunRotate.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+using UnityEngine;
+
+public class SunRotate : MonoBehaviour
+{
+    public float rotate = 0.5f;
+    private Transform trans;
+    // Use this for initialization
+    void Start()
+    {
+        trans = GetComponent<Transform>();
+        
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        Time.timeScale = 1.0f;
+        trans.transform.Rotate(rotate * Time.fixedDeltaTime , 0, 0,Space.Self);
+    }
+}
