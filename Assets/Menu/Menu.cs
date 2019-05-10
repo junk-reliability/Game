8ef33e109481c511e86c6757398284b3808e7f47
Skrals <39524682+Skrals@users.noreply.github.com>
2019-05-11 01:38:58 +0300

    Главное меню
    Начата разработка главного меню

---
+++ Assets/Menu/Menu.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Menu : MonoBehaviour
+{
+
+    public void StartGame()
+    {
+#pragma warning disable CS0618 // Тип или член устарел
+        Application.LoadLevel(1);
+#pragma warning restore CS0618 // Тип или член устарел
+    }
+    public void LoadGame()
+    {
+
+    }
+    public void Settings()
+    {
+
+    }
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
